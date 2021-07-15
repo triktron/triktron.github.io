0e17383837ffde4cdf64e3ad55b4a820a255b4e0
Triktron <ele.trik@hotmail.com>
2021-07-15 10:52:26 +0200

    small change to card video script

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,9 +1,9 @@
-window.addEventListener("load", function() {
+// window.addEventListener("load", function() {
   document.querySelectorAll('video').forEach((vid, i) => {
-    vid.parentElement.addEventListener("mouseover", () => vid.play());
+    vid.parentElement.addEventListener("mouseover", () => {vid.currentTime = 0;vid.play();});
   });
 
   document.querySelectorAll('video').forEach((vid, i) => {
     vid.parentElement.addEventListener("mouseleave", () => vid.pause());
   });
-});
+// });
